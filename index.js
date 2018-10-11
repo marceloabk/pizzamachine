9b5579b2302447549247cadeae1e5335b9eefc65
Marcelo Cristiano <marceloabk@hotmail.com>
2018-10-11 05:42:11 -0300

    Adicionando conexao com postgres

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const { Client } = require('pg')
+const client = new Client({
+  user: 'postgres',
+  host: 'db'
+})
+
+const PORT = process.env.PORT || 5000;
+
+(async function(){
+  await client.connect()
+
+  const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
+  
+})();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
