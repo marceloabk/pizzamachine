9140bf734c9753e138fbe420e311975dc7cab134
Thiago Lima <thiagolima.unb@gmail.com>
2018-10-12 19:41:56 -0300

    Adding login route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const authRoutes = require('./routes/auth-routes')
 const { Client } = require('pg')
 
 const client = new Client({
@@ -23,4 +24,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/login', (req, res) => res.render('pages/login'))
+  .use('/auth', authRoutes)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
