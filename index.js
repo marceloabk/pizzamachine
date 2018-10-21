40737d3e504c81736a066a17a8852b3adb65f109
Marcelo Cristiano <marceloabk@hotmail.com>
2018-10-21 17:47:16 -0300

    Setuping socket.io

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const path = require('path')
 const authRoutes = require('./routes/auth-routes')
+const usersRouter = require('./routes/users')
 const passportSetup = require('./config/passport-setup')
-const usersRouter = require("./routes/users")
-
 const express = require('express')
-const app =  express()
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 const PORT = process.env.PORT | 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('pages/index')
 })
+
 app.get('/login', (req, res) => {
   res.render('pages/login')
+})
+
+app.post('/order', (req, res) => {
+  var pizza = req.body.pizza
+  io.sockets.emit('rasp', JSON.stringify(pizza))
+  res.json(pizza)
 })
 
 app.use('/auth', authRoutes)
 app.use('/users', usersRouter)
 
-app.listen(PORT, () => console.log(`Such pizza on ${ PORT }`))
+http.listen(PORT, () => console.log(`Such pizza on ${ PORT }`))
