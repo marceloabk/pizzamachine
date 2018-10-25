92e76a499a37b8a3c8bea1678101e0c336f30bd3
Marcelo Cristiano <marceloabk@hotmail.com>
2018-10-25 14:11:49 -0300

    Fixing deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-const PORT = process.env.PORT | 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
