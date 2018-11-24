cdb5b6367719b8c3b6370d17e557973b6d7906ce
Marcelo Cristiano <marceloabk@hotmail.com>
2018-11-24 11:22:41 -0200

    knexfile

--- app/knexfile.js
+++ app/knexfile.js
@@ -4,15 +4,14 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://pjmqmuqzysggad:3e9f47a1b60e228df7c615d3fa0e424f390c7de2cc423145c811c59e4de0116e@ec2-50-17-203-51.compute-1.amazonaws.com:5432/decpi61mn42q6k',
-    migrations: {
-      tableName: 'migrations'
+    connection: {
+      user: 'postgres',
+      host: 'db',
+      database: 'pizza',
     },
-    ssl: true
-    //connection: {
-    //  user: 'postgres',
-    //  host: 'db',
-    //  database: 'pizza',
-    //},
   },
+  production: {
+    client: 'postgresql',
+    connection: 'postgres://pjmqmuqzysggad:3e9f47a1b60e228df7c615d3fa0e424f390c7de2cc423145c811c59e4de0116e@ec2-50-17-203-51.compute-1.amazonaws.com:5432/decpi61mn42q6k',
+  }
 }
