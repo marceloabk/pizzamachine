5dc48bc7c7e0c17db1e68ef02617e060c1fb857a
Marcelo Cristiano <marceloabk@hotmail.com>
2018-11-24 18:05:45 +0000

    Finishing rasp server

--- rasp/server.js
+++ rasp/server.js
@@ -1,13 +1,13 @@
 const io = require('socket.io-client')
-// const SPI = require('spi')
-//var socket = io('https://pizza-machine-staging.herokuapp.com')
-var socket = io('http://localhost:5000/')
+const SPI = require('spi')
+var socket = io('https://pizza-machine-staging.herokuapp.com')
+//var socket = io('http://localhost:5000/')
 
 
-// var spi = new SPI.Spi('/dev/spidev0.0', {
-//     'mode': SPI.MODE['MODE_0'],  // always set mode as the first option
-//     'chipSelect': SPI.CS['none'] // 'none', 'high' - defaults to low
-//   }, function(s){s.open();});
+ var spi = new SPI.Spi('/dev/spidev0.0', {
+     'mode': SPI.MODE['MODE_0'],  // always set mode as the first option
+     'chipSelect': SPI.CS['none'] // 'none', 'high' - defaults to low
+   }, function(s){s.open();});
  
 var txbuf = new Buffer([0x55])
 var rxbuf = new Buffer([0x00])
@@ -17,35 +17,38 @@ socket.emit('askOrder', '')
 socket.on('getOrder', function(msg, callback){
     //msg = JSON.parse(msg)
     console.log(`Fazer pizza: ${JSON.stringify(msg)}`)
-    setTimeout(function() {
-        callback(msg)
-    },2000)
+    sendPizzaToMSP(msg.order.pizza_id)
+    isPizzaDone(callback, msg)
+    //setTimeout(function() {
+    //    callback(msg)
+    //},1000)
 })
 
 socket.on('updatedDb', function() {
     socket.emit('askOrder', '')
 })
 
-// function isPizzaDone(callback) {
-//     setTimeout(function(){
-//         spi.read(rxbuf, function(device, buf) {
-//             console.log('LEU')
-//             let mspResponse = buf.toString('hex')
-//             console.log(mspResponse)
+ function isPizzaDone(callback, msg) {
+     setTimeout(function(){
+         spi.read(rxbuf, function(device, buf) {
+             console.log('LEU')
+             let mspResponse = buf.toString('hex')
+             console.log(mspResponse)
 
-//             if (mspResponse == 'A') {
-//                 console.log('Enviar resposta: pizza feita')
-//                 callback('Pizza feita')
-//             } else {
-//                 isPizzaDone(callback)
-//             }
-//         })
-//     }, 10000)
-// }
+             if (mspResponse == 'ff') {
+                 console.log('Enviar resposta: pizza feita')
+                 callback(msg)
+             } else {	
+                 isPizzaDone(callback, msg)
+             }
+         })
+     }, 8000)
+ }
 
 function sendPizzaToMSP(pizzaNumber){
     pizzaNumber = `0x0` + pizzaNumber
-    txbuf = new Buffer(pizzaNumber)
+    console.log(pizzaNumber)
+    txbuf = new Buffer([pizzaNumber])
     spi.write(txbuf, function(device, buf) {
         console.log('ESCREVEU')
         console.log(buf.toString('hex'))
